7f01ba433f043d0dbba6b914fc94bb90b0c22756
“Ashwin <ashwinvidiyala@gmail.com>
2018-03-29 16:17:54 -0500

    Separate out Slack Authorization into different file

--- chirpy_daddy.rb
+++ chirpy_daddy.rb
@@ -1,36 +1,31 @@
 require 'slack-ruby-client'
 
-Slack.configure do |config|
-  config.token = ENV['SLACK_API_TOKEN']
-  raise 'Missing ENV[SLACK_API_TOKEN]!' unless config.token
+require_relative 'slack_authorization'
+
+slack_client = Slack::RealTime::Client.new
+
+slack_client.on :hello do
+  puts "Successfully connected, welcome '#{slack_client.self.name}' to the '#{slack_client.team.name}' team at https://#{slack_client.team.domain}.slack.com."
 end
 
-client = Slack::RealTime::Client.new
-
-client.on :hello do
-  puts "Successfully connected, welcome '#{client.self.name}' to the '#{client.team.name}' team at https://#{client.team.domain}.slack.com."
-end
-
-client.on :message do |data|
+slack_client.on :message do |data|
   puts data
   case data.text
   when 'chirpy hi' then
-    client.web_client.chat_postMessage channel: data.channel, text: "Hi <@#{data.user}>!", as_user: true
+    slack_client.web_client.chat_postMessage channel: data.channel, text: "Hi <@#{data.user}>!", as_user: true
   when /^chirpy.#(.*)/ then
-    client.web_client.chat_postMessage channel: data.channel, text: "Your Hashtag is #{$1}!", as_user: true
+    slack_client.web_client.chat_postMessage channel: data.channel, text: "Your Hashtag is #{$1}!", as_user: true
   when /^chirpy/ then
-    client.web_client.chat_postMessage channel: data.channel, text: "Sorry <@#{data.user}>, what?", as_user: true
+    slack_client.web_client.chat_postMessage channel: data.channel, text: "Sorry <@#{data.user}>, what?", as_user: true
   end
 end
 
-client.on :close do |_data|
+slack_client.on :close do |_data|
   puts "Client is about to disconnect"
 end
 
-client.on :closed do |_data|
+slack_client.on :closed do |_data|
   puts "Client has disconnected successfully!"
 end
 
-
-
-client.start!
+slack_client.start!
